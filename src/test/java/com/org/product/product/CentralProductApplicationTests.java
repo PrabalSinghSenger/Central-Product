e4e2d3568b8716ad4a0e7fb1de20aa1122d73b83
PrabalSinghSenger <ppsingh.singh2@gmail.com>
2022-02-03 21:54:05 +0530

    initial commit

---
+++ src/test/java/com/org/product/product/CentralProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.product.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
